c9d96167a61262768445e767fbb500df490399df
Marzhan <nurl-imarzhan@mail.ru>
2021-01-27 21:35:09 +0600

    Initial Commit

---
+++ Manapoly Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Manapoly Dice
+//
+//  Created by tse on 1/27/21.
+//  Copyright © 2021 marzhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
